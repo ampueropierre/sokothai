a9c1c96111f034799de7ab3f0eac1040414bd47f
ampueropierre <ampuero.pierre@gmail.com>
2020-11-03 20:00:49 +0100

    create fixtures

---
+++ sokothai/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@sokothai.fr');
+        $user->setPassword($this->encoder->encodePassword($user,'sokothai_pass'));
+        $manager->persist($user);
+
+        for ($i = 1; $i <= 3; $i++) {
+            $dateTime = new \DateTime('now');
+            $event = new Event();
+            $event->setName('Event0'.$i);
+            $event->setUser($user);
+            $event->setCreatedAt($i > 1 ? $dateTime->modify('+'.$i.' month') : $dateTime);
+            $event->setUrlFacebook('https://www.facebook.com/');
+            $manager->persist($event);
+        }
+
+        $manager->flush();
+    }
+}
