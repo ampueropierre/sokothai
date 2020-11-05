ae1ebe8c1fb029a11e631db4a0476ffdf3f0084d
ampueropierre <ampuero.pierre@gmail.com>
2020-11-05 23:30:52 +0100

    Send mail, add bundle SwiftMailer, Update HomeController and template

--- sokothai/src/Controller/HomeController.php
+++ sokothai/src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,15 +14,50 @@ class HomeController extends AbstractController
 
     /**
      * @Route("/",name="index_homepage")
-     *
+     * @param Request $request
+     * @param \Swift_Mailer $mailer
      * @return Response
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
         $events = $this->getDoctrine()->getRepository(Event::class)->findAll();
 
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = (new \Swift_Message($form->getData()['subject']))
+                ->setFrom($form->getData()['mail'])
+                ->setTo($this->getParameter('mail_contact'))
+                ->setReplyTo($form->getData()['mail'])
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig',[
+                            'message' => $form->getData()['text'],
+                            'name_contact' => $form->getData()['name'],
+                            'mail_contact' => $form->getData()['mail'],
+                        ]
+                    ),
+                    'text/html'
+                )
+                ->addPart(
+                    $this->renderView(
+                        'emails/contact.txt.twig',[
+                            'message' => $form->getData()['text'],
+                            'name_contact' => $form->getData()['name'],
+                            'mail_contact' => $form->getData()['mail'],
+                        ]
+                    ),
+                    'text/plain'
+                )
+            ;
+
+            $mailer->send($message);
+        }
+
         return $this->render('homepage/index.html.twig',[
-            'events' => $events
+            'events' => $events,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ sokothai/src/Form/ContactType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mail', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Le champ ne peut être vide']),
+                    new Email(['message' => 'Adresse Email non valide'])
+                ],
+                'attr' => [
+                  'placeholder' => '* Votre email'
+                ],
+                'translation_domain' => false
+            ])
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Le champ ne peut être vide']),
+                ],
+                'attr' => [
+                    'placeholder' => '* Votre nom'
+                ],
+                'translation_domain' => false
+            ])
+            ->add('subject', ChoiceType::class, [
+                'choices' => [
+                    'Information Général' => 'Information Général',
+                    'Réservation' => 'Réservation',
+                    'Privatisation Devis' => 'Privatisation Devis',
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Le champ ne peut être vide']),
+                ],
+                'translation_domain' => false
+            ])
+            ->add('text', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 3,'minMessage' => 'Minimum de 3 caractères']),
+                ],
+                'attr' => [
+                    'placeholder' => '* Votre message'
+                ],
+                'translation_domain' => false
+            ])
+        ;
+    }
+}
